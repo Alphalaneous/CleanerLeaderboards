adf361fc378b861d4f0d904ce924225779572338
Ashton <38200084+Alphalaneous@users.noreply.github.com>
2026-07-05 19:59:59 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Cleaner Leaderboards
+
+Cleans up the Leaderboard UI.
+
+This UI is based on https://geometrydash.com/leaderboards
---
+++ src/main.cpp
@@ -0,0 +1,192 @@
+#include <Geode/Geode.hpp>
+#include <Geode/modify/GJScoreCell.hpp>
+#include <Geode/modify/CustomListView.hpp>
+
+using namespace geode::prelude;
+
+class $modify(MyGJScoreCell, GJScoreCell) {
+
+	static void onModify(auto& self) {
+		(void) self.setHookPriorityPost("GJScoreCell::loadFromScore", Priority::VeryLate);
+	}
+
+	CCNode* createStatContainer(ZStringView ID) {
+		auto container = CCNode::create();
+
+		container->setLayout(SimpleRowLayout::create()
+			->setGap(5.f)
+			->setMainAxisScaling(AxisScaling::None)
+			->setCrossAxisScaling(AxisScaling::None)
+			->setMainAxisAlignment(MainAxisAlignment::Start)
+		);
+
+		container->setContentSize({70, m_height / 2 - 5});
+
+		CCNode* parent = m_mainLayer;
+
+		Ref<CCNode> label = parent->getChildByID(fmt::format("{}-label", ID));
+		if (!label) {
+			parent = m_mainLayer->getChildByID("stats-menu");
+			if (!parent) return container;
+			label = parent->getChildByID(fmt::format("{}-label", ID));
+		}
+		Ref<CCNode> icon = parent->getChildByID(fmt::format("{}-icon", ID));
+
+		if (!label || !icon) return container;
+
+		label->removeFromParent();
+		icon->removeFromParent();
+
+		auto iconContainer = CCNode::create();
+		iconContainer->setID(fmt::format("{}-icon-container", ID));
+		iconContainer->setContentSize({20, 20});
+		iconContainer->addChild(icon);
+
+		icon->setPosition(iconContainer->getContentSize() / 2);
+
+		static_cast<CCLabelBMFont*>(label.data())->limitLabelWidth(60, 0.35f, 0.01f);
+
+		container->addChild(iconContainer);
+		container->addChild(label);
+		container->setID(fmt::format("{}-container", ID));
+
+		container->setScale(0.8f);
+
+		container->updateLayout();
+
+		return container;
+	}
+
+    void loadFromScore(GJUserScore* score) {
+		GJScoreCell::loadFromScore(score);
+
+		m_mainLayer->setContentSize({m_width, m_height});
+
+		auto rankLabel = m_mainLayer->getChildByID("rank-label");
+		if (rankLabel) {
+			rankLabel->setPosition({20, m_height / 2.f + 2});
+			static_cast<CCLabelBMFont*>(rankLabel)->limitLabelWidth(24, 0.5f, 0.01f);
+		}
+
+		auto player = m_mainLayer->getChildByID("player-icon");
+		if (!player) {
+			player = m_mainLayer->getChildByID("cvolton.betterinfo/player-icon-menu");
+		}
+
+		if (player) {
+			player->setPosition({50, m_height / 2.f});
+			player->setContentSize({0, 0});
+			player->setScale(0.65f);
+		}
+
+		auto mainMenu = m_mainLayer->getChildByID("main-menu");
+		if (mainMenu) {
+			mainMenu->setContentSize({m_width - 100, m_height});
+			mainMenu->ignoreAnchorPointForPosition(false);
+			mainMenu->setAnchorPoint({0, 0.5});
+			mainMenu->setScale(0.62f);
+			mainMenu->setLayout(SimpleRowLayout::create()
+				->setMainAxisAlignment(MainAxisAlignment::Start)
+			);
+			mainMenu->updateLayout();
+			mainMenu->setPosition({68, m_height / 2.f + 2});
+		}
+
+		auto statsContainer = CCNode::create();
+		statsContainer->setContentSize({180, m_height - 10});
+		statsContainer->setAnchorPoint({1.f, 0.5f});
+		statsContainer->setPosition({m_width - 5, m_height / 2.f});
+		statsContainer->setID("stats-container");
+
+		statsContainer->setLayout(RowLayout::create()
+			->setCrossAxisOverflow(false)
+			->setAutoScale(false)
+			->setGrowCrossAxis(true)
+			->setAxisAlignment(AxisAlignment::Start)
+		);
+
+		auto stars = createStatContainer("stars");
+		auto moons = createStatContainer("moons");
+		auto demons = createStatContainer("demons");
+		auto userCoins = createStatContainer("user-coins");
+
+		statsContainer->addChild(stars);
+		statsContainer->addChild(moons);
+		statsContainer->addChild(demons);
+		statsContainer->addChild(createStatContainer("diamonds"));
+		statsContainer->addChild(userCoins);
+		statsContainer->addChild(createStatContainer("coins"));
+
+		switch(m_score->m_leaderboardStat) {
+			case LeaderboardStat::Stars:
+				stars->setZOrder(-1);
+				break;
+			case LeaderboardStat::Moons:
+				moons->setZOrder(-1);
+				break;
+			case LeaderboardStat::Demons:
+				demons->setZOrder(-1);
+				break;
+			case LeaderboardStat::UserCoins:
+				userCoins->setZOrder(-1);
+				break;
+		}
+
+		auto creatorPointsContainer = CCNode::create();
+		creatorPointsContainer->setLayout(SimpleColumnLayout::create()
+			->setGap(5.f)
+			->setMainAxisScaling(AxisScaling::None)
+			->setCrossAxisScaling(AxisScaling::None)
+			->setMainAxisAlignment(MainAxisAlignment::Center)
+		);
+		creatorPointsContainer->setID("creator-points-container");
+
+		creatorPointsContainer->setContentSize({20, m_height});
+		creatorPointsContainer->setAnchorPoint({1.f, 0.5f});
+		creatorPointsContainer->setPosition({statsContainer->getPositionX() - statsContainer->getScaledContentWidth() - 2, m_height / 2});
+		creatorPointsContainer->setScale(0.8f);
+
+		auto statsMenu = m_mainLayer->getChildByID("stats-menu");
+		if (statsMenu) {
+			Ref<CCNode> creatorPointIcon = statsMenu->getChildByID("creator-points-icon");
+			Ref<CCNode> creatorPointLabel = statsMenu->getChildByID("creator-points-label");
+
+			if (creatorPointIcon && creatorPointLabel) {
+				creatorPointIcon->removeFromParent();
+				creatorPointLabel->removeFromParent();
+
+				static_cast<CCLabelBMFont*>(creatorPointLabel.data())->limitLabelWidth(20, 0.35f, 0.01f);
+
+				creatorPointsContainer->addChild(creatorPointIcon);
+				creatorPointsContainer->addChild(creatorPointLabel);
+				
+				creatorPointsContainer->updateLayout();
+
+				m_mainLayer->addChild(creatorPointsContainer);
+			}
+		}
+
+		statsContainer->updateLayout();
+		m_mainLayer->addChild(statsContainer);
+
+	}
+};
+
+class $modify(MyCustomListView, CustomListView) {
+    TableViewCell* getListCell(char const* identifier) {
+		auto ret = CustomListView::getListCell(identifier);
+
+		if (m_type == BoomListType::Score) {
+			ret->m_height = 35.f;
+		}
+
+		return ret;
+	}
+
+    static float getCellHeight(BoomListType type) {
+		if (type == BoomListType::Score) {
+			return 35.f;
+		}
+		return CustomListView::getCellHeight(type);
+	}
+};
